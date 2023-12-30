08093e186e568c7d1b5873c1ab7b52fa6fc89a72
Tarun Jangra <51834686+tarunjangra4@users.noreply.github.com>
2023-12-30 13:55:47 +0530

    changes

--- app.js
+++ app.js
@@ -102,11 +102,12 @@ if (
     key: fs.readFileSync("/cert/matrix24app.ca.key"),
     cert: fs.readFileSync("/cert/matrix24app.ca.crt"),
   };
-
+  console.log("if https");
   const server = https.createServer(options, app).listen(443, function () {
     console.log("Express server listening on port " + 443);
   });
 } else {
+  console.log("else http");
   const server = http.createServer({}, app).listen(PORT, function () {
     console.log("Express server listening on port " + PORT);
   });
