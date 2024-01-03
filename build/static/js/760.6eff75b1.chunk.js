f3c1dea7e32cb3e24ab4548c21bc582cad140a93
Tarun Jangra <51834686+tarunjangra4@users.noreply.github.com>
2024-01-03 17:18:01 +0530

    frontend build file added

---
+++ build/static/js/760.6eff75b1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkvideo_web_app=self.webpackChunkvideo_web_app||[]).push([[760],{23760:function(e,a,p){p(72791);a.Z=p.p+"static/media/loginPageImage.8b85e6d53f762120fc372650150e267a.svg"}}]);
+//# sourceMappingURL=760.6eff75b1.chunk.js.map
